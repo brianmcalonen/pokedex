4fe29131cc5a725fcc607628344f50e460d06589
brianmcalonen <brianm0623@gmail.com>
2019-06-18 12:18:40 -0700

    added searchPokemon and resetData functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,97 @@
 $(document).ready(function() {
-  console.log( "ready!" )
 
   let randomNumber = Math.floor(Math.random() * 151) + 1;
 
-  console.log( randomNumber )
+  $(".randomButton").on("click", function() {
 
-  var queryURL = "https://pokeapi.co/api/v2/pokemon/56";
+    randomNumber = Math.floor(Math.random() * 151) + 1;
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    $(".pokemonSprite").attr("src", response.sprites.front_default);
-    $(".pokemonName").text(response.name);
-    $(".pokemonId").text(response.id);
-    $('.pokemonTypes').text(`Type: ${response.types[0].type.name}`);
-    $('.pokemonHeight').text(`Height:<br/>${response.height}m`);
-    $('.pokemonWeight').text(`Weight:<br/>${response.weight}lbs`);
-    // $('.pokemonDescription').text(`${response}`);
-    console.log(response)
-    $('.pokemonStats').append(`HP:  ${response.stats[5].base_stat}<br/>`);
-    $('.pokemonStats').append(`Attack:  ${response.stats[4].base_stat}<br/>`);
-    $('.pokemonStats').append(`Defense:  ${response.stats[3].base_stat}<br/>`);
-    $('.pokemonStats').append(`Sp.Atk:  ${response.stats[2].base_stat}<br/>`);
-    $('.pokemonStats').append(`Sp.Def:  ${response.stats[1].base_stat}<br/>`);
-    $('.pokemonStats').append(`Speed:  ${response.stats[0].base_stat}`);
+    console.log(randomNumber);
 
-
-    // Name
-    console.log(response.name);
-    // Sprites
-    console.log(response.sprites.front_default);
-    // Id
-    console.log(response.id);
-    // Types
-    console.log(response.types[0].type.name);
-    // Height
-    console.log("height");
-    console.log(response.height);
-    // Weight
-    console.log("weight");
-    console.log(response.weight);
-    // Hit Points
-    console.log("HP");
-    console.log(response.stats[5].base_stat);
-    // Attack
-    console.log("Attack");
-    console.log(response.stats[4].base_stat);
-    // Defense
-    console.log("Defense");
-    console.log(response.stats[3].base_stat);
-    // Speed
-    console.log("Speed");
-    console.log(response.stats[0].base_stat);
-    // Special Attack 
-    console.log("Special Attack");
-    console.log(response.stats[2].base_stat);
-    // Special Defense
-    console.log("Special Defense");
-    console.log(response.stats[1].base_stat);
-
-  });
-
-  var flavorURL = "https://pokeapi.co/api/v2/pokemon-species/56";
-
-  $.ajax({
-    url: flavorURL,
-    method: "GET"
-  }).then(function(response){
-    var flavorText = response.flavor_text_entries[2].flavor_text;
-    $(".pokemonDescription").text(flavorText);
+    resetData();
+    searchPokemon(randomNumber);
+    
   })
+
+  function searchPokemon(num) {
+    var queryURL = "https://pokeapi.co/api/v2/pokemon/" + num;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+  
+      var pokeHeight = Math.round(((response.height) / 10) * 10) / 10;
+      var pokeWeight = Math.round((((response.weight) * 2.20462) / 10) * 10) / 10;
+  
+      $(".pokemonSprite").attr("src", response.sprites.front_default);
+      $(".pokemonName").text(response.name);
+      $(".pokemonId").text(response.id);
+      $('.pokemonTypes').text(`Type: ${response.types[0].type.name}`);
+      $('.pokemonHeight').text(`Height: ${pokeHeight} m`);
+      $('.pokemonWeight').text(`Weight: ${pokeWeight} lbs`);
+      $('.pokemonStats').append(`HP:  ${response.stats[5].base_stat}<br/>`);
+      $('.pokemonStats').append(`Attack:  ${response.stats[4].base_stat}<br/>`);
+      $('.pokemonStats').append(`Defense:  ${response.stats[3].base_stat}<br/>`);
+      $('.pokemonStats').append(`Sp.Atk:  ${response.stats[2].base_stat}<br/>`);
+      $('.pokemonStats').append(`Sp.Def:  ${response.stats[1].base_stat}<br/>`);
+      $('.pokemonStats').append(`Speed:  ${response.stats[0].base_stat}`);
+    });
+  
+    var flavorURL = "https://pokeapi.co/api/v2/pokemon-species/" + num;
+  
+    $.ajax({
+      url: flavorURL,
+      method: "GET"
+    }).then(function(response){
+      var flavorText = response.flavor_text_entries[2].flavor_text;
+      $(".pokemonDescription").text(flavorText);
+    })
+  }
+
+  function resetData() {
+    $(".pokemonStats").text("");
+  }
+
+  searchPokemon(randomNumber);
 
 
 // End of $(document).ready(function(){})
 });
+
+// // Name
+// console.log(response.name);
+// // Sprites
+// console.log(response.sprites.front_default);
+// // Id
+// console.log(response.id);
+// // Types
+// console.log(response.types[0].type.name);
+// // Height
+// console.log("height");
+// console.log(response.height);
+// // Weight
+// console.log("weight");
+// console.log(response.weight);
+// // Hit Points
+// console.log("HP");
+// console.log(response.stats[5].base_stat);
+// // Attack
+// console.log("Attack");
+// console.log(response.stats[4].base_stat);
+// // Defense
+// console.log("Defense");
+// console.log(response.stats[3].base_stat);
+// // Speed
+// console.log("Speed");
+// console.log(response.stats[0].base_stat);
+// // Special Attack 
+// console.log("Special Attack");
+// console.log(response.stats[2].base_stat);
+// // Special Defense
+// console.log("Special Defense");
+// console.log(response.stats[1].base_stat);
+
 
 // $(function() {
 //   console.log('js is connected')
